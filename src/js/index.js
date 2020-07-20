3b855c277fa8f925db127e480773fa401f90e1b7
Marcin Krygier <krygier.mar@gmail.com>
2020-07-20 22:03:10 +0200

    github icon fix, js loading of repositories, links in new tab, hand animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,60 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-console.log('Hello 🚀')
+console.log("Hi, I am Marcin - nce to meet you 🚀");
+
+fetch("https://api.github.com/users/marcinkrygier/repos?sort=created")
+  .then((res) => res.json())
+  .then((res) => {
+    const container = document.querySelector(".projects-grid--js");
+    for (let repo of res) {
+      const { description, homepage, html_url, name } = repo;
+      const template = `<article class="project">
+        <div class="project__window">
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+        </div>
+        <div class="project__content">
+          <img src="/img/github.svg" alt="" />
+          <h3 class="project__grid project__title">
+            <span class="project__label">project:</span>
+            <span>${name}</span>
+          </h3>
+          <p class="project__grid project__grid--description">
+            <span class="project__label">description:</span
+            ><span>${description}</span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">demo:</span
+            ><span
+              >&lt;<a
+              target="_blank"
+              rel="noopener noreferrer"
+                class="project__link"
+                href="${homepage}"
+                title="${name} - demo"
+                >see_here</a
+              >&gt;</span
+            >
+          </p>
+          <p class="project__grid">
+            <span class="project__label">github:</span
+            ><span
+              >&lt;<a
+                class="project__link"
+                href="${html_url}"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="${name} - code"
+                >source_code</a
+              >&gt;</span
+            >
+          </p>
+        </div>
+      </article>`;
+      if (description) {
+        container.innerHTML += template;
+      }
+    }
+  })
+  .catch((e) => console.log(e));
